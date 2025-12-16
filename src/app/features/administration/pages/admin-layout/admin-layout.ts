36fa13e88b284cbc93dfc45d6379c30b4973e7db
josel <jos23limbert@gmail.com>
2025-12-16 18:07:18 -0400

    implementacion pagina de inicio

---
+++ src/app/features/administration/pages/admin-layout/admin-layout.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-layout',
+  imports: [],
+  templateUrl: './admin-layout.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class AdminLayout {}
