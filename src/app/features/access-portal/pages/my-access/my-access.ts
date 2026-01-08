61b548c382f25b5c73d509f33e177b12df80ab14
josel <jos23limbert@gmail.com>
2026-01-08 18:18:46 -0400

    correcion vista home

--- src/app/features/access-portal/pages/my-access/my-access.ts
+++ src/app/features/access-portal/pages/my-access/my-access.ts
@@ -1,7 +1,8 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
-import { AccessDataSource } from '../../services';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
+import { CommonModule } from '@angular/common';
+
+import { AccessDataSource } from '../../services';
 
 @Component({
   selector: 'app-my-access',
@@ -12,29 +13,9 @@ import { toSignal } from '@angular/core/rxjs-interop';
 export default class MyAccess {
   private accesDataSource = inject(AccessDataSource);
 
-  access = toSignal(this.accesDataSource.getMyApplicationms());
-
-
-  systems = signal<any[]>([
-    {
-      id: '1',
-      name: 'ERP Corporativo',
-      description: 'Gestión de recursos, finanzas y facturación electrónica.',
-      imageUrl: 'assets/icons/erp.svg',
-      url: 'https://erp.tuempresa.com',
-    },
-    {
-      id: '2',
-      name: 'Gestión de RRHH',
-      description: 'Control de asistencia, vacaciones y nómina de empleados.',
-      imageUrl: 'assets/icons/hr.svg',
-      url: 'https://hr.tuempresa.com',
-    },
-    // ... más apps
-  ]);
+  applications = toSignal(this.accesDataSource.getMyApplicationms());
 
   openApp(url: string) {
-    // Abre en una nueva pestaña
     window.open(url, '_blank');
   }
 }
